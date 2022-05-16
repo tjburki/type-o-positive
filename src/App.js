5c2f8a80fb8f91f70a0f232e89ded7f5d9c30826
Tyler Burki <tylerjburki@gmail.com>
2022-05-16 12:52:59 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typer from './components/Typer';
 
 function App() {
+  const [text, setText] = useState('It was 7 minutes after midnight. The dog was lying on the grass in the middle of the lawn in front of Mrs Shears’ house. Its eyes were closed. It looked as if it was running on its side, the way dogs run when they think they are chasing a cat in a dream. But the dog was not running or asleep. The dog was dead. There was a garden fork sticking out of the dog. The points of the fork must have gone all the way through the dog and into the ground because the fork had not fallen over. I decided that the dog was probably killed with the fork because I could not see any other wounds in the dog and I do not think you would stick a garden fork into a dog after it had died for some other reason, like cancer for example, or a road accident. But I could not be certain about this.');
+  const [playing, setPlaying] = useState(false);
+  const [results, setResults] = useState(null);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Type-o Positive
       </header>
+      <div>
+        {
+           !playing && results === null && <div style={{textAlign: 'center'}}><button onClick={() => setPlaying(true)}>Start</button></div>
+        }
+              { (playing || results !== null) && <Typer
+                text={text}
+                playing={playing}
+                gameOver={results => { setResults(results); setPlaying(false); }}
+              /> }
+                    {
+                !playing && results !== null && <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', bottom: 0, top: 0, left: 0, right: 0, position: 'fixed', zIndex: 10, backgroundColor: 'transparent'}}>
+                  <div style={{backgroundColor: 'black', color: 'white', padding: 15}}>
+                  <div style={{marginBottom: 15}}>WPM: {results.wpm}</div>
+                  <div style={{marginBottom: 15}}>Errors: {results.errors}</div>
+                  <div style={{marginBottom: 15}}>Adjusted WPM: {results.adjusted}</div>
+                  <div style={{textAlign: 'center'}}><button onClick={() => setPlaying(true)}>Play Again</button></div>
+                  </div>
+                </div>
+            }
+      </div>
     </div>
   );
 }
---
+++ src/components/Typer.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+
+function Typer({text, playing, gameOver}) {
+    const [type, setType] = useState('');
+    const [startTime, setStartTime] = useState(null);
+    const [currentTime, setCurrentTime] = useState(null);
+    const [to, setTo] = useState(null);
+    const timeLimit = 60;
+    
+
+    useEffect(() => {
+        if (!playing) return;
+        setType('');
+        setStartTime(new Date());
+        setTo(setInterval(() => setCurrentTime(new Date()), 1000));
+        document.getElementById('typer').focus();
+        document.querySelector(`#char-1`).scrollIntoView({
+            behavior: 'smooth'
+          });
+    }, [playing]);
+
+    useEffect(() => {
+        if (!startTime || !currentTime || timeLimit - timeElapsed() > 0) return;
+
+        clearInterval(to);
+        if (!type.length) {
+            gameOver({wpm: 0, errors: 0, adjusted: 0});
+            return;
+        }
+        const wpm = text.slice(0, type.length).match(/ /g).length;
+        const errors = Object.assign([], type).filter((ty, i) => ty !== text[i]).length;
+        const adjusted = Math.max(Math.floor(wpm - (.75 * errors)), 0);
+        gameOver({wpm, errors, adjusted});
+    }, [currentTime])
+
+    function timeElapsed() {
+        return Math.round((currentTime.getTime() - startTime.getTime()) / 1000);
+    }
+
+    function onKeyDown(e) {
+        if (!playing) return;
+        if (e.keyCode === 8) {
+            setType(type.slice(0, type.length - 1));
+            return;
+        }
+
+        if (e.key.length !== 1) {
+            return;
+        }
+
+        if (type.length >= text.length) return;
+    
+        setType(`${type}${e.key}`);
+
+        e.preventDefault();
+        document.querySelector(`#char-${type.length}`).scrollIntoView({
+            behavior: 'smooth'
+          });
+    }
+
+    return (
+        <div tabindex="0" id="typer" style={{display: 'flex'}} onKeyDown={onKeyDown}>
+            {playing && startTime && currentTime && <div style={{position: 'fixed', bottom: 15, right: 15, padding: 15, backgroundColor: 'black'}}>{
+                timeLimit - timeElapsed()
+            }</div>}
+            {
+                Object.assign([], text).map((c, i) => <div key={i} id={`char-${i}`} >
+                    
+                    <div style={{marginBottom: 10, color: i === type.length ? '#F9DC5C' : 'inherit', position: 'relative'}}>
+                    {
+                        i === type.length ? <div style={{color: 'F9DC5C', position: 'absolute', bottom: '-25px'}}>^</div> : null
+                    }
+                    {
+                    c === ' ' ? <span>&nbsp;</span> : <span>{c}</span>}</div>
+                    <div style={{marginBottom: 20, color: i < type.length ? type[i] === text[i] ? '#3185FC' : '#E84855' : 'transparent'}}>{type[i] || <span>&nbsp;</span>}</div>
+                </div>)
+            }
+        </div>
+    );
+}
+
+export default Typer;
